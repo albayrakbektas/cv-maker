da8379f9483214b79437339093f7c716881a4a7c
Bektas Albayrak <albayrakbektas44@gmail.com>
2022-11-07 15:11:35 +0300

    Create ZoomButton.vue

---
+++ src/components/button/ZoomButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <i @click="$emit('event-handler')" class="fa-solid fa-expand f-xl" />
+</template>
+
+<script>
+export default {
+  name: "ZoomButton",
+};
+</script>
+
+<style scoped lang="scss">
+i {
+  position: relative;
+  width: fit-content;
+  padding: 1rem;
+  border: 1px solid transparent;
+  background-image: none;
+  transition: all 0.4s ease-in-out;
+  border-radius: 8px;
+  cursor: pointer;
+  color: #ffffff;
+  &:hover {
+    border: 1px solid #8b8b8b;
+    background-image: linear-gradient(
+      to right,
+      rgba(255, 0, 0, 0.1),
+      rgba(255, 0, 0, 0.3)
+    );
+  }
+}
+</style>
